5cd109d5c0e962cfb16d301789fe8f45c2568171
Anthony Shellman <atshellman@gmail.com>
2023-11-11 16:55:12 -0700

    Add function for duplicating image objects

--- src/App.vue
+++ src/App.vue
@@ -2,25 +2,23 @@
 import CardsContainer from './components/CardsContainer.vue'
 import { ref } from 'vue'
 
-let id = 0
-const images = ref([
-  {id: id++, imageURL: `Image ${id}`},
-  {id: id++, imageURL: `Image ${id}`},
-  {id: id++, imageURL: `Image ${id}`},
-  {id: id++, imageURL: `Image ${id}`},
-  {id: id++, imageURL: `Image ${id}`},
-  {id: id++, imageURL: `Image ${id}`},
-  {id: id++, imageURL: `Image ${id}`},
-  {id: id++, imageURL: `Image ${id}`},
-  {id: id++, imageURL: `Image ${id}`},
-  {id: id++, imageURL: `Image ${id}`},
-  {id: id++, imageURL: `Image ${id}`},
-  {id: id++, imageURL: `Image ${id}`},
-  {id: id++, imageURL: `Image ${id}`},
-  {id: id++, imageURL: `Image ${id}`},
-  {id: id++, imageURL: `Image ${id}`},
-  {id: id++, imageURL: `Image ${id}`},
-  ])
+
+const urls = ['Image_1', 'Image_2','Image_3','Image_4','Image_5','Image_6','Image_7','Image_8']
+
+  const imageList = (urlList: Array<string>): Array<Object> => {
+    let id = 0
+    const imageBuilder = urlList?.reduce((acc:Array<Object>, curr:string): Array<Object> => {
+      acc.push({id: id++, imageURL: curr}) && acc.push({id: id++, imageURL: curr})
+      return acc
+    }, [])
+    return imageBuilder
+  }
+
+  const imageRandomizer = imageList(urls).map((image:object, index:number) => {
+    // Need to shuffle the indices of the result of imageList(urls)
+  })
+
+  const images = ref(imageList(urls)) // Put imageRandomizer here
 
   defineProps({
     images: Array<{id: number, imageURL: string}>
