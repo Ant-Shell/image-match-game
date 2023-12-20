65cc62c71116e7c0994d76ef219f5dc6918f1689
Anthony Shellman <atshellman@gmail.com>
2023-12-20 16:44:28 -0700

    Remove comments, update props

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,14 +1,13 @@
 <script lang="ts" setup>
 
 defineProps({
-  // image: Object,
   shuffledImage: Object
 })
 </script>
 
 <template>
   <section class="w-fit h-fit">
-    {{ shuffledImage?.imageURL }}
+    {{ shuffledImage?.url }}
   </section> 
 </template>
 
