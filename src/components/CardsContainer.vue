20819d3ca081a0a10efe051d861151401e3a0322
Anthony Shellman <atshellman@gmail.com>
2023-12-21 17:09:49 -0700

    Adjust styling

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -9,7 +9,7 @@ defineProps({
 
 
 <template>
-  <section class="grid grid-cols-4 place-items-center h-screen">
+  <section class="grid gap-4 grid-cols-4 place-items-center h-full w-full p-8 bg-[#1f2e27]">
     <div v-for="shuffledImage in shuffledImages">
       <Card v-bind:shuffledImage="shuffledImage"/>
     </div>
