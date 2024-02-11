7b3658ab59e9dca45171a3a26022208ecb3d1fe1
Anthony Shellman <atshellman@gmail.com>
2024-02-10 20:20:04 -0700

    Add test click event

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,10 +3,14 @@
 defineProps({
   shuffledImage: Object
 })
+
+const test = ():void => {
+    console.log("Hello!")
+}
 </script>
 
 <template>
-  <section className="border-black border-2 h-full w-full rounded-lg">
+  <section className="border-black border-2 h-full w-full rounded-lg" @click="test">
       <img className="h-full w-full rounded-lg" :src="shuffledImage?.src.medium">
   </section> 
 </template>
