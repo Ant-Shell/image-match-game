874fe3f9c1fb3e2c78b8353ae8506e9c5683de14
Anthony Shellman <atshellman@gmail.com>
2024-02-10 19:59:39 -0700

    Add styling for breakpoints

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -9,7 +9,7 @@ defineProps({
 
 
 <template>
-  <section className="grid gap-4 grid-cols-4 p-8 h-screen w-screen">
+  <section className="grid gap-4 grid-cols-4 p-8 h-screen md:h-[120vh] lg:h-[250vh] xl:h-[260vh] w-screen">
     <div v-for="shuffledImage in shuffledImages" className="w-full h-full">
       <Card v-bind:shuffledImage="shuffledImage"/>
     </div>
