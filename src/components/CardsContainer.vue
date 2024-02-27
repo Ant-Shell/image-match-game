2a162d359aa5894d4da116a88d4d58773f39399e
Anthony Shellman <atshellman@gmail.com>
2024-02-27 10:20:48 -0700

    Add and pass props

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -2,16 +2,16 @@
 import Card from './Card.vue'
 
 defineProps({
-  shuffledImages: Array<{id: number, src: { medium: string }}>
+  shuffledImages: Array<{id: number, src: { medium: string }}>,
+  clickedCards: Array<string>
 })
-
 </script>
 
 
 <template>
   <section className="grid gap-4 grid-cols-4 p-8 h-[120vh] sm:h-[130vh] md:h-[170vh] lg:h-[250vh] xl:h-[290vh] 2xl:h-[400vh] w-screen">
     <div v-for="shuffledImage in shuffledImages" className="w-full h-full">
-      <Card v-bind:shuffledImage="shuffledImage"/>
+      <Card v-bind:shuffledImage="shuffledImage" v-bind:clickedCards="clickedCards"/>
     </div>
   </section>
 </template>
