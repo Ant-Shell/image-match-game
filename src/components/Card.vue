c3df32500fd40476bbdf61767fafe43feaf7acc1
Anthony Shellman <atshellman@gmail.com>
2024-02-26 18:25:01 -0700

    Add addCard and checkForMatch functions

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,16 +1,36 @@
 <script lang="ts" setup>
 
-defineProps({
-  shuffledImage: Object
+const props = defineProps({
+  shuffledImage: Object,
+  clickedCards: Array<string>
 })
 
-const test = ():void => {
-    console.log("Hello!")
+const addCard = (event:Event):void => {
+    if (props.clickedCards !== undefined && props.clickedCards?.length < 2) {
+      props.clickedCards.push((event.target as HTMLImageElement).src)
+    }
+    checkForMatch()
+}
+
+const checkForMatch = () => {
+  if (props.clickedCards === undefined) {
+    return
+  }
+
+  if (props.clickedCards[0] === props.clickedCards[1]) { // Cards match
+    props.clickedCards.length = 0
+    // console.log("Hello")
+    // If cards match, leave cards face up
+  } else if (props.clickedCards.length === 2) { // Cards don't match
+    props.clickedCards.length = 0
+    // console.log("Hi")
+    // If cards do not match, flip cards face down again
+  }
 }
 </script>
 
 <template>
-  <section className="border-black border-2 h-full w-full rounded-lg" @click="test">
+  <section className="border-black border-2 h-full w-full rounded-lg" @click="addCard">
       <img className="h-full w-full rounded-lg" :src="shuffledImage?.src.medium">
   </section> 
 </template>
