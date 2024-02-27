196ce2b33e822967aa0375afa3198c2f96957541
Anthony Shellman <atshellman@gmail.com>
2024-02-27 12:02:09 -0700

    Add keys for cards

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -1,17 +1,16 @@
 <script lang="ts" setup>
 import Card from './Card.vue'
 
-defineProps({
+const props = defineProps({
   shuffledImages: Array<{id: number, src: { medium: string }}>,
   clickedCards: Array<string>
 })
 </script>
 
-
 <template>
   <section className="grid gap-4 grid-cols-4 p-8 h-[120vh] sm:h-[130vh] md:h-[170vh] lg:h-[250vh] xl:h-[290vh] 2xl:h-[400vh] w-screen">
-    <div v-for="shuffledImage in shuffledImages" className="w-full h-full">
-      <Card v-bind:shuffledImage="shuffledImage" v-bind:clickedCards="clickedCards"/>
+    <div v-for="shuffledImage in shuffledImages" :key="props.shuffledImages?.indexOf(shuffledImage)" className="w-full h-full">
+      <Card v-bind:shuffledImage="shuffledImage" v-bind:clickedCards="clickedCards" />
     </div>
   </section>
 </template>
