cece1f4b6eb90556a34b93e1a7bb734e0e0d3e57
Anthony Shellman <atshellman@gmail.com>
2024-02-27 18:02:09 -0700

    Add Image type, use index for key

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -1,16 +1,23 @@
 <script lang="ts" setup>
 import Card from './Card.vue'
 
-const props = defineProps({
-  shuffledImages: Array<{id: number, src: { medium: string }}>,
-  clickedCards: Array<string>
+type Image  = {
+    id: number,
+    src: { medium: string },
+    isClicked?: boolean
+  }
+
+defineProps({
+  shuffledImages: Array<Image>,
+  clickedCards: Array<string>,
+  matchedCards: Array<string>
 })
 </script>
 
 <template>
   <section className="grid gap-4 grid-cols-4 p-8 h-[120vh] sm:h-[130vh] md:h-[170vh] lg:h-[250vh] xl:h-[290vh] 2xl:h-[400vh] w-screen">
-    <div v-for="shuffledImage in shuffledImages" :key="props.shuffledImages?.indexOf(shuffledImage)" className="w-full h-full">
-      <Card v-bind:shuffledImage="shuffledImage" v-bind:clickedCards="clickedCards" />
+    <div v-for="(shuffledImage, index) in shuffledImages" v-bind:key="index" className="w-full h-full">
+      <Card v-bind:shuffledImage="shuffledImage" v-bind:clickedCards="clickedCards" v-bind:matchedCards="matchedCards" />
     </div>
   </section>
 </template>
