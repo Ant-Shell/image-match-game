b878f940288243de9b8a6b07b0e4da4b784864aa
Anthony Shellman <atshellman@gmail.com>
2024-02-28 14:23:04 -0700

    Add id for use as key

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -1,22 +1,22 @@
 <script lang="ts" setup>
 import Card from './Card.vue'
 
-type Image  = {
+  type Image  = {
     id: number,
     src: { medium: string },
     isClicked?: boolean
   }
 
-defineProps({
-  shuffledImages: Array<Image>,
-  clickedCards: Array<string>,
-  matchedCards: Array<string>
-})
+  defineProps({
+    shuffledImages: Array<Image>,
+    clickedCards: Array<string>,
+    matchedCards: Array<string>
+  })
 </script>
 
 <template>
   <section className="grid gap-4 grid-cols-4 p-8 h-[120vh] sm:h-[130vh] md:h-[170vh] lg:h-[250vh] xl:h-[290vh] 2xl:h-[400vh] w-screen">
-    <div v-for="(shuffledImage, index) in shuffledImages" v-bind:key="index" className="w-full h-full">
+    <div v-for="shuffledImage in shuffledImages" v-bind:key="shuffledImage.id" className="w-full h-full">
       <Card v-bind:shuffledImage="shuffledImage" v-bind:clickedCards="clickedCards" v-bind:matchedCards="matchedCards" />
     </div>
   </section>
