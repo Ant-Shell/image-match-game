5dbe3d65ee70280311e27bbe810139c26bf79582
Anthony Shellman <atshellman@gmail.com>
2024-02-28 14:32:10 -0700

    Remove test code, add flipCard function and conditional render

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -6,39 +6,22 @@ const props = defineProps({
   clickedCards: Array<string>,
 })
 
-const addCard = (event:Event):void => {
-    if (props.clickedCards !== undefined && props.clickedCards.length < 2) {
-      props.clickedCards.push((event.target as HTMLImageElement).src)
+  const flipCard = (event:Event) => {
+    if (props.shuffledImage === undefined) {
+      return
     }
-    checkForMatch()
-}
 
-const checkForMatch = () => {
-  if (props.clickedCards === undefined) {
-    return
+    if ((event.target as HTMLImageElement).alt === props.shuffledImage.id.toString()) {
+      props.shuffledImage.isClicked = true
+    }
   }
-
-  if (props.clickedCards[0] === props.clickedCards[1]) { // Cards match, reset clickedCards array
-    props.clickedCards.length = 0
-    // console.log("Hello")
-    // If cards match, leave cards face up
-  } else if (props.clickedCards.length === 2) { // Cards don't match, reset clickedCards array
-    props.clickedCards.length = 0
-    // console.log("Hi")
-    // If cards do not match, flip cards face down again
-  }
-}
-
-// const test = () => {
-//   console.log(props.shuffledImage?.isClicked)
-// }
-
 </script>
 
 <template>
   <section className="border-black border-2 h-full w-full rounded-lg hover:border-sky-700
-  hover:mt-[-5px] hover:shadow-md hover:shadow-cyan-500/50 cursor-pointer" @click="addCard">
-      <!-- <img className="h-full w-full rounded-lg" :src="shuffledImage?.src.medium"> -->
-      <img className="h-full w-full rounded-lg" :src="cardBlue">
+  hover:mt-[-5px] hover:shadow-md hover:shadow-cyan-500/50 cursor-pointer" @click="flipCard">
+    <img v-if="props.shuffledImage?.isClicked" className="h-full w-full rounded-lg"
+    :src="shuffledImage?.src.medium" :alt="props.shuffledImage?.id">
+    <img v-else className="h-full w-full rounded-lg" :src="cardBlue" :alt="props.shuffledImage?.id">
   </section> 
 </template>
