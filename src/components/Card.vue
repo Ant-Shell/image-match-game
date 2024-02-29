411c5c2fc01b398e3d96216e5328bf4dbb254cd2
Anthony Shellman <atshellman@gmail.com>
2024-02-29 14:42:55 -0700

    Adjut variable name, add prop and placeholder functions

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -8,26 +8,27 @@ interface Photo {
   }
 
 const props = defineProps({
-  shuffledImage: Object,
+  shuffledPhoto: Object,
   clickedCards: Array<Photo>,
-  matchedCards: Array<Photo>
+  matchedCards: Array<Photo>,
+  shuffledPhotos: Array<Photo>,
 })
 
   const flipCard = (event:Event) => {
-    if (props.shuffledImage === undefined) {
+    if (props.shuffledPhoto === undefined) {
       return
     }
 
-    if ((event.target as HTMLImageElement).alt === props.shuffledImage.id.toString()) {
-      props.shuffledImage.isClicked = true
+    if ((event.target as HTMLImageElement).alt === props.shuffledPhoto.id.toString()) {
+      props.shuffledPhoto.isClicked = true
     }
     addCard()
   }
 
   const addCard = () => {
-    const photo = props.shuffledImage as Photo
+    const photo = props.shuffledPhoto as Photo
 
-    if (props.shuffledImage === undefined) {
+    if (props.shuffledPhoto === undefined) {
       return
     }
 
@@ -38,28 +39,42 @@ const props = defineProps({
   }
 
   const checkForMatch = () => {
-    if (props.clickedCards === undefined) {
+    if (props.clickedCards === undefined || props.shuffledPhoto === undefined) {
       return
     }
 
     if (props.clickedCards?.length === 2)
       if (props.clickedCards[0].src.medium === props.clickedCards[1].src.medium) {
         console.log("It's a match!")
+        // Cards stay face up
+
         props.clickedCards.length = 0
       } else {
         console.log("Not a match")
+        // Flip cards back over - face down
+        console.log(props.clickedCards[0].id)
+        console.log(props.clickedCards[1].id)
         props.clickedCards.length = 0
       }
       console.log(props.clickedCards)
+      // console.log(props.matchedCards)
   }
+
+  // const cardResetter = () => {
+
+  // }
+
+  // const cardMatcher = () => {
+
+  // }
 
 </script>
 
 <template>
   <section className="border-black border-2 h-full w-full rounded-lg" @click="flipCard">
-    <img v-if="props.shuffledImage?.isClicked" className="h-full w-full rounded-lg"
-    :src="shuffledImage?.src.medium" :alt="props.shuffledImage?.id">
+    <img v-if="props.shuffledPhoto?.isClicked" className="h-full w-full rounded-lg"
+    :src="shuffledPhoto?.src.medium" :alt="props.shuffledPhoto?.id">
     <img v-else className="h-full w-full rounded-lg hover:mt-[-5px] hover:shadow-md
-     hover:border-sky-700 hover:shadow-cyan-500/50 cursor-pointer" :src="cardBlue" :alt="props.shuffledImage?.id">
+     hover:border-sky-700 hover:shadow-cyan-500/50 cursor-pointer" :src="cardBlue" :alt="props.shuffledPhoto?.id">
   </section> 
 </template>
