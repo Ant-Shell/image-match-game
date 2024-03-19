20c36560d1f5f1b76a45c9aecae8982ae02b3553
Anthony Shellman <atshellman@gmail.com>
2024-03-19 15:58:00 -0600

    Initial commit

---
+++ src/components/Heading.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+const props = defineProps({
+  gameResetter: Function,
+})
+</script>
+
+<template>
+  <header className="flex justify-evenly items-center w-screen h-[10%]">
+    <button className="w-28 border-2 bg-[#3a3d2c] border-[#3a3d2c] text-white
+    hover:bg-[#78807c] rounded-full" @click="props.gameResetter">
+      Reset Game
+    </button>
+    <div>
+      <h2>Matches:</h2>
+      <h2>Total Moves:</h2>
+    </div>
+  </header>
+</template>
