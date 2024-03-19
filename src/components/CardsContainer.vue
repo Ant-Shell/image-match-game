a5f850b8bd82e00c2c5b03b4498866761a2ca82a
Anthony Shellman <atshellman@gmail.com>
2024-03-19 15:57:32 -0600

    Adjust styling for mobile

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -16,7 +16,7 @@ defineProps({
 </script>
 
 <template>
-  <section className="grid gap-4 grid-cols-4 p-8 h-[120vh] sm:h-[130vh] md:h-[170vh] lg:h-[250vh] xl:h-[290vh] 2xl:h-[400vh] w-screen">
+  <section className="grid gap-4 grid-cols-4 p-4 w-screen h-[90%]">
     <div v-for="shuffledPhoto in shuffledPhotos" v-bind:key="shuffledPhoto.id" className="w-full h-full">
       <Card v-bind:shuffledPhoto="shuffledPhoto" v-bind:clickedCards="clickedCards" v-bind:shuffledPhotos="shuffledPhotos"/>
     </div>
