9e8a038ce21a483a400f3ca0325296a2573b2101
Anthony Shellman <atshellman@gmail.com>
2024-03-21 15:44:03 -0600

    Change positioning of button and game details

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -7,13 +7,13 @@ const props = defineProps({
 
 <template>
   <header class="flex justify-evenly items-center h-[10%] w-screen md:w-[20%] md:flex-col lg:w-[15%]">
-    <button class="w-28 border-2 bg-[#3a3d2c] border-[#3a3d2c] text-white
+    <div class="text-white">
+      <h2>Matches: 1</h2>
+      <h2>Total Moves: 2</h2>
+    </div>
+    <button class="w-28 md:w-36 border-2 bg-[#3a3d2c] border-[#3a3d2c] text-white
     hover:bg-[#78807c] rounded-md" @click="props.gameResetter">
       Reset Game
     </button>
-    <div>
-      <h2>Matches:</h2>
-      <h2>Total Moves:</h2>
-    </div>
   </header>
 </template>
