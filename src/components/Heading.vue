06e18a37b68f9b704373406e742469cc36181f97
Anthony Shellman <atshellman@gmail.com>
2024-03-21 15:30:00 -0600

    Adjust layout on med and lg devices

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -6,7 +6,7 @@ const props = defineProps({
 </script>
 
 <template>
-  <header class="flex justify-evenly items-center w-screen h-[10%]">
+  <header class="flex justify-evenly items-center h-[10%] w-screen md:w-[20%] md:flex-col lg:w-[15%]">
     <button class="w-28 border-2 bg-[#3a3d2c] border-[#3a3d2c] text-white
     hover:bg-[#78807c] rounded-md" @click="props.gameResetter">
       Reset Game
