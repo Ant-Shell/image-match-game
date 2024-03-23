2aaf35321ab6b6090ab08a6915f4f8d87ea4da1e
Anthony Shellman <atshellman@gmail.com>
2024-03-23 16:56:53 -0600

    Update from id to position where applicable

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -12,7 +12,7 @@ const flipCard = (event:Event) => {
     return
   }
 
-  if ((event.target as HTMLImageElement).alt === props.shuffledPhoto.id.toString()) {
+  if ((event.target as HTMLImageElement).alt === props.shuffledPhoto.position.toString()) {
     props.shuffledPhoto.isClicked = true
   }
   props.addCard?.(props.shuffledPhoto)
@@ -22,10 +22,10 @@ const flipCard = (event:Event) => {
 <template>
   <button class="border-black border-2 h-full w-full rounded-lg" :disabled="props.shuffledPhoto?.isLocked" @click="flipCard">
     <img v-if="props.shuffledPhoto?.isClicked" class="h-full w-full rounded-lg cursor-default"
-    :src="shuffledPhoto?.src.medium" :alt="props.shuffledPhoto?.id">
+    :src="shuffledPhoto?.src.medium" :alt="props.shuffledPhoto?.position">
     <img v-else-if="props.shuffledPhoto?.isMatched" class="h-full w-full rounded-lg cursor-default"
-    :src="shuffledPhoto?.src.medium" :alt="props.shuffledPhoto?.id">
+    :src="shuffledPhoto?.src.medium" :alt="props.shuffledPhoto?.position">
     <img v-else class="h-full w-full rounded-lg hover:mt-[-5px] hover:shadow-md
-     hover:border-sky-700 hover:shadow-cyan-500/50 cursor-pointer" :src="cardBlue" :alt="props.shuffledPhoto?.id">
+     hover:border-sky-700 hover:shadow-cyan-500/50 cursor-pointer" :src="cardBlue" :alt="props.shuffledPhoto?.position">
   </button>
 </template>
