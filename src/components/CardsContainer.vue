fd6a362377cb52e686af4ccc41ffef01df392b30
Anthony Shellman <atshellman@gmail.com>
2024-03-23 16:58:18 -0600

    Add position property to Photo interface, update from id to position where applicable

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -4,6 +4,7 @@ import Card from './Card.vue'
 interface Photo {
   id: number,
   src: { medium: string },
+  position?: number,
   isClicked?: boolean,
   isMatched?: boolean,
   isLocked?: boolean,
@@ -17,7 +18,7 @@ const props = defineProps({
 
 <template>
   <section class="grid grid-cols-4 gap-1 justify-items-center h-full p-2 lg:pt-1 lg:pb-1 overflow-y-auto">
-    <div v-for="shuffledPhoto in props.shuffledPhotos" v-bind:key="shuffledPhoto.id" class="w-20 h-36 md:w-[8rem] md:h-[15rem]
+    <div v-for="shuffledPhoto in props.shuffledPhotos" v-bind:key="shuffledPhoto.position" class="w-20 h-36 md:w-[8rem] md:h-[15rem]
       lg:w-[13rem] lg:h-[21rem] xl:w-[8rem] xl:h-[12rem] 2xl:w-[9rem] 2xl:h-[13rem]">
       <Card v-bind:shuffledPhoto="shuffledPhoto" :addCard="props.addCard"/>
     </div>
