599fed89c5dfb34323fed4c2b90e3c516414f1d6
Anthony Shellman <atshellman@gmail.com>
2024-03-23 13:50:49 -0600

    Assign props variable, remove clickedCards prop, add addCard prop

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -9,17 +9,17 @@ interface Photo {
   isLocked?: boolean,
 }
 
-defineProps({
+const props = defineProps({
   shuffledPhotos: Array<Photo>,
-  clickedCards: Array<Photo>,
+  addCard: Function,
 })
 </script>
 
 <template>
   <section class="grid grid-cols-4 gap-1 justify-items-center h-full p-2 lg:pt-1 lg:pb-1 overflow-y-auto">
-    <div v-for="shuffledPhoto in shuffledPhotos" v-bind:key="shuffledPhoto.id" class="w-20 h-36 md:w-[8rem] md:h-[15rem]
+    <div v-for="shuffledPhoto in props.shuffledPhotos" v-bind:key="shuffledPhoto.id" class="w-20 h-36 md:w-[8rem] md:h-[15rem]
       lg:w-[13rem] lg:h-[21rem] xl:w-[8rem] xl:h-[12rem] 2xl:w-[9rem] 2xl:h-[13rem]">
-      <Card v-bind:shuffledPhoto="shuffledPhoto" v-bind:clickedCards="clickedCards" v-bind:shuffledPhotos="shuffledPhotos"/>
+      <Card v-bind:shuffledPhoto="shuffledPhoto" :addCard="props.addCard"/>
     </div>
   </section>
 </template>
