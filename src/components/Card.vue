ff8db50bb674d04c1ab1fdde4c5c7950eafdb5bd
Anthony Shellman <atshellman@gmail.com>
2024-03-24 11:39:38 -0600

    Update prop from shuffledPhoto to shuffledCard

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -3,29 +3,29 @@ import cardBlue from '../assets/Card_Background_Blue.png'
 
 
 const props = defineProps({
-  shuffledPhoto: Object,
+  shuffledCard: Object,
   addCard: Function,
 })
 
 const flipCard = (event:Event) => {
-  if (props.shuffledPhoto === undefined) {
+  if (props.shuffledCard === undefined) {
     return
   }
 
-  if ((event.target as HTMLImageElement).alt === props.shuffledPhoto.position.toString()) {
-    props.shuffledPhoto.isClicked = true
+  if ((event.target as HTMLImageElement).alt === props.shuffledCard.position.toString()) {
+    props.shuffledCard.isClicked = true
   }
-  props.addCard?.(props.shuffledPhoto)
+  props.addCard?.(props.shuffledCard)
 }
 </script>
 
 <template>
-  <button class="border-black border-2 h-full w-full rounded-lg" :disabled="props.shuffledPhoto?.isLocked" @click="flipCard">
-    <img v-if="props.shuffledPhoto?.isClicked" class="h-full w-full rounded-lg cursor-default"
-    :src="shuffledPhoto?.src.medium" :alt="props.shuffledPhoto?.position">
-    <img v-else-if="props.shuffledPhoto?.isMatched" class="h-full w-full rounded-lg cursor-default"
-    :src="shuffledPhoto?.src.medium" :alt="props.shuffledPhoto?.position">
+  <button class="border-black border-2 h-full w-full rounded-lg" :disabled="props.shuffledCard?.isLocked" @click="flipCard">
+    <img v-if="props.shuffledCard?.isClicked" class="h-full w-full rounded-lg cursor-default"
+    :src="shuffledCard?.src.medium" :alt="props.shuffledCard?.position">
+    <img v-else-if="props.shuffledCard?.isMatched" class="h-full w-full rounded-lg cursor-default"
+    :src="shuffledCard?.src.medium" :alt="props.shuffledCard?.position">
     <img v-else class="h-full w-full rounded-lg hover:mt-[-5px] hover:shadow-md
-     hover:border-sky-700 hover:shadow-cyan-500/50 cursor-pointer" :src="cardBlue" :alt="props.shuffledPhoto?.position">
+     hover:border-sky-700 hover:shadow-cyan-500/50 cursor-pointer" :src="cardBlue" :alt="props.shuffledCard?.position">
   </button>
 </template>
