a0ca1d86d353e475ce21381c2c7e32a4d1a006e8
Anthony Shellman <atshellman@gmail.com>
2024-03-24 11:41:20 -0600

    Update interface from Photo to Card, update props to match

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -1,7 +1,7 @@
 <script lang="ts" setup>
 import Card from './Card.vue'
 
-interface Photo {
+interface Card {
   id: number,
   src: { medium: string },
   position?: number,
@@ -11,16 +11,16 @@ interface Photo {
 }
 
 const props = defineProps({
-  shuffledPhotos: Array<Photo>,
+  shuffledCards: Array<Card>,
   addCard: Function,
 })
 </script>
 
 <template>
   <section class="grid grid-cols-4 gap-1 justify-items-center h-full p-2 lg:pt-1 lg:pb-1 overflow-y-auto">
-    <div v-for="shuffledPhoto in props.shuffledPhotos" v-bind:key="shuffledPhoto.position" class="w-20 h-36 md:w-[8rem] md:h-[15rem]
+    <div v-for="shuffledCard in props.shuffledCards" v-bind:key="shuffledCard.position" class="w-20 h-36 md:w-[8rem] md:h-[15rem]
       lg:w-[13rem] lg:h-[21rem] xl:w-[8rem] xl:h-[12rem] 2xl:w-[9rem] 2xl:h-[13rem]">
-      <Card v-bind:shuffledPhoto="shuffledPhoto" :addCard="props.addCard"/>
+      <Card v-bind:shuffledCard="shuffledCard" :addCard="props.addCard"/>
     </div>
   </section>
 </template>
