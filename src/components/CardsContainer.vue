0c53cada11d5198688bf27978be04431e1b72b50
Anthony Shellman <atshellman@gmail.com>
2024-03-25 16:22:40 -0600

    Adjust sizing at breakpoints for larger screens

--- src/components/CardsContainer.vue
+++ src/components/CardsContainer.vue
@@ -19,7 +19,7 @@ const props = defineProps({
 <template>
   <section class="grid grid-cols-4 gap-1 justify-items-center h-full p-2 lg:pt-1 lg:pb-1 overflow-y-auto">
     <div v-for="shuffledCard in props.shuffledCards" v-bind:key="shuffledCard.position" class="w-20 h-36 md:w-32 md:h-60
-      lg:w-52 lg:h-80 xl:w-32 xl:h-48 2xl:w-36 2xl:h-52">
+      lg:w-42 lg:h-70 xl:w-32 xl:h-60">
       <Card v-bind:shuffledCard="shuffledCard" :addCard="props.addCard"/>
     </div>
   </section>
