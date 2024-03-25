b2e37f5e316894d9621a14da0e204af9f1e62a68
Anthony Shellman <atshellman@gmail.com>
2024-03-25 14:39:26 -0600

    Add props for matchCount and moveCount, display where applicable

--- src/components/Heading.vue
+++ src/components/Heading.vue
@@ -1,6 +1,8 @@
 <script setup lang="ts">
 
 const props = defineProps({
+  matchCount: Number,
+  moveCount: Number,
   gameResetter: Function,
 })
 </script>
@@ -8,8 +10,8 @@ const props = defineProps({
 <template>
   <header class="flex justify-evenly items-center h-[10%] md:h-[20%] w-screen md:w-[20%] md:flex-col lg:w-[15%]">
     <div class="text-white md:text-xl lg:text-2xl">
-      <h2>Matches: 1</h2>
-      <h2>Total Moves: 2</h2>
+      <h2>Matches: {{ props.matchCount }} / 8</h2>
+      <h2>Total Moves: {{ props.moveCount }}</h2>
     </div>
     <button class="w-28 md:w-36 h-10 md:h-12 lg:h-16 xl:h-12 border-2 bg-[#3a3d2c] border-[#3a3d2c] text-white md:text-xl lg:text-2xl
     hover:bg-[#78807c] rounded-md" @click="props.gameResetter">
