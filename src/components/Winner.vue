0c9beac279a9ec8fa0fd455b018fbdcfc74e66d4
Anthony Shellman <atshellman@gmail.com>
2024-04-03 16:16:07 -0600

    Add responsiveness for div and button

--- src/components/Winner.vue
+++ src/components/Winner.vue
@@ -4,11 +4,11 @@
 <template>
   <section class="z-10 absolute w-full h-full bg-[#7f7972]/50">
     <div class="text-white md:text-xl lg:text-2xl w-full h-full flex flex-col items-center justify-center">
-      <div class="w-60 h-60 flex flex-col items-center justify-evenly bg-[#7f7972] rounded-md">
+      <div class="w-60 h-60 md:w-72 md:h-72 md:ml-[20%] lg:w-80 lg:h-80 flex flex-col items-center justify-evenly bg-[#7f7972] rounded-lg">
         <h1 class="text-2xl">
           You Win!
         </h1>
-        <button class="w-28 md:w-36 h-10 md:h-12 border-2 bg-[#374F2F] border-[#304529] text-white md:text-xl lg:text-2xl
+        <button class="w-28 md:w-36 lg:w-40 h-10 md:h-12  border-2 bg-[#374F2F] border-[#304529] text-white md:text-xl lg:text-2xl
           hover:bg-[#4A6741] rounded-md">
           Restart Game
         </button>
