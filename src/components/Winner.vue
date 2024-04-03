91a1edf3c5a1fdf7c74078eafcced6552b359846
Anthony Shellman <atshellman@gmail.com>
2024-04-03 15:49:11 -0600

    Initial commit

---
+++ src/components/Winner.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section class="z-10 absolute w-full h-full bg-[#7f7972]/50">
+    <div class="text-white md:text-xl lg:text-2xl w-full h-full flex flex-col items-center justify-center">
+      <div class="w-60 h-60 flex flex-col items-center justify-evenly bg-[#7f7972] rounded-md">
+        <h1 class="text-2xl">
+          You Win!
+        </h1>
+        <button class="w-28 md:w-36 h-10 md:h-12 border-2 bg-[#374F2F] border-[#304529] text-white md:text-xl lg:text-2xl
+          hover:bg-[#4A6741] rounded-md">
+          Restart Game
+        </button>
+      </div>
+    </div>
+  </section>
+</template>
