fbeb3b18318f520857109c29585aef33b9da18d6
Anthony Shellman <atshellman@gmail.com>
2024-04-05 19:31:47 -0600

    Initial commit

---
+++ src/utils/apiCalls.ts
@@ -0,0 +1,22 @@
+import { createClient, Photos } from 'pexels';
+
+const API_KEY = import.meta.env.VITE_APP_API_KEY;
+const client = createClient(API_KEY);
+
+const getImages = async (): Promise<Photos> => {
+  try {
+    const response = await client.photos.curated({ per_page: 8 });
+    if ('photos' in response) {
+      return response as Photos;
+    } else {
+      const errorMessage = response
+      console.error('Error fetching photos:', errorMessage.error);
+      throw new Error(errorMessage.error);
+    }
+  } catch (error) {
+    console.error("Failed to fetch curated photos:", error);
+    throw error;
+  }
+};
+
+export { getImages };
