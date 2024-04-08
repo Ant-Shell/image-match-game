eefa6264fdf364240395aeea1bd625e20ec92bfd
Anthony Shellman <atshellman@gmail.com>
2024-04-08 16:11:24 -0600

    Initial commit

---
+++ src/components/Error.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+const props = defineProps({
+  errorMessage: String,
+})
+</script>
+
+<template>
+  <section v-if="props.errorMessage" class="h-[10%] md:h-[5%] w-screen z-10 fixed bottom-0 bg-red-500/50
+   flex items-center justify-center text-white whitespace-pre md:whitespace-normal">
+    <p>{{ props.errorMessage }}</p>
+  </section>
+</template>
