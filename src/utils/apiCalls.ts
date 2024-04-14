09a5fa3cae3a8a32d98c3a0cfe39be767741ba49
Anthony Shellman <atshellman@gmail.com>
2024-04-14 11:52:47 -0600

    Update getImages to migrate away from pexels client library

--- src/utils/apiCalls.ts
+++ src/utils/apiCalls.ts
@@ -1,21 +1,22 @@
-import { createClient, Photos } from 'pexels';
-
 const API_KEY = import.meta.env.VITE_APP_API_KEY;
-const client = createClient(API_KEY);
 
-const getImages = async (): Promise<Photos> => {
+const getImages = async () => {
   try {
-    const response = await client.photos.curated({ per_page: 8 });
-    if ('photos' in response) {
-      return response as Photos;
-    } else {
-      const errorMessage = response
-      throw new Error(errorMessage.error);
+    const response = await fetch("https://api.pexels.com/v1/curated?per_page=8", {
+      headers: {
+        Authorization: API_KEY
+      }
+    })
+    if (!response.ok) {
+      throw new Error(response.statusText)
     }
+    const data = await response.json()
+    return data
+
   } catch (error) {
-    console.error("Failed to fetch curated photos:", error);
-    throw error;
+    console.log(error)
+    throw error
   }
-};
+}
 
 export { getImages };
